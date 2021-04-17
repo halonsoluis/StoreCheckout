7ae5b00f582ab7e67b5fdf4dfd3bc9ee84ea6ddd
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 21:06:41 +0200

    Refactor Bulk discount test suite for better readability

--- StoreCheckoutTests/BulkDiscountOfferTests.swift
+++ StoreCheckoutTests/BulkDiscountOfferTests.swift
@@ -13,7 +13,7 @@ class BulkDiscount: Offer {
     private let newPrice: Float
     private let code: String
 
-    init(minimumAmount: Int = 3, ofItemWithCode code: String = "TSHIRT", newPrice: Float = 19) {
+    init(minimumAmount: Int, ofItemWithCode code: String, newPrice: Float) {
         self.minimumAmount = minimumAmount
         self.newPrice = newPrice
         self.code = code
@@ -38,32 +38,35 @@ class BulkDiscountOfferTests: XCTestCase {
     let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
     let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
     let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-    let mug2 = StoreProduct(code: "MUG-2", name: "Coffee Mug", price: 7.5)
 
     func test_offer_returnsNoDiscountOnEmptyBasket() throws {
-        assert(discount: BulkDiscount(minimumAmount: 3), for: [], is: 0)
+        assert(discount: createSUT(), for: [], is: 0)
     }
 
     func test_offer_returnsZeroOnDifferentProducts() throws {
-        assert(discount: BulkDiscount(minimumAmount: 3), for: [voucher, tShirt, mug], is: 0)
+        assert(discount: createSUT(), for: [voucher, tShirt, mug], is: 0)
     }
 
-    func test_offer_returnsZeroWhenRepeatedProductsAmountIsNotOVerTheMinimum() throws {
-        assert(discount: BulkDiscount(minimumAmount: 3), for: [voucher, voucher, mug, mug2, mug], is: 0)
+    func test_offer_returnsZeroWhenRepeatedProductsAmountIsNotOverTheMinimum() throws {
+        assert(discount: createSUT(), for: [tShirt, tShirt, voucher, voucher, voucher, mug, mug, mug], is: 0)
     }
 
-    func test_offer_returnsDiscountOfRepeatedProducts() throws {
-        assert(discount: BulkDiscount(minimumAmount: 3), for: [tShirt, tShirt, tShirt], is: 3)
+    func test_offer_returnsDiscountOfTheMinimumOfRepeatedProducts() throws {
+        assert(discount: createSUT(), for: [tShirt, tShirt, tShirt], is: 3)
     }
-//
-//    func test_offer_returnsDiscountForTuplesOfRepeatedProducts() throws {
-//        assert(discount: BulkDiscount(minimumAmount: 3), for: [voucher, voucher, voucher, mug, mug2, mug], is: 12.5)
-//    }
+
+    func test_offer_returnsDiscountForMoreThanTheMinimumOfRepeatedProducts() throws {
+        assert(discount: createSUT(), for: [tShirt, tShirt, tShirt, tShirt, tShirt, tShirt], is: 6)
+    }
 
     // MARK: - Helpers
 
     func assert(discount: Offer, for products: [StoreProduct], is finalPrice: Float, file: StaticString = #file, line: UInt = #line) {
         XCTAssertEqual(discount.discount(over:products), finalPrice, file: file, line: line)
+    }
+
+    func createSUT() -> Offer {
+        BulkDiscount(minimumAmount: 3, ofItemWithCode: "TSHIRT", newPrice: 19)
     }
 
 }
