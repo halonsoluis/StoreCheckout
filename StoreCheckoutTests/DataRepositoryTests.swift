f41437c0745ac52200270cb5f562e9151b3624af
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 09:04:34 +0200

    Remove redundant implementation of test

--- StoreCheckoutTests/DataRepositoryTests.swift
+++ StoreCheckoutTests/DataRepositoryTests.swift
@@ -1,6 +1,6 @@
 //
-//  StoreCheckoutTests.swift
-//  StoreCheckoutTests
+//  DataRepositoryTests.swift
+//  DataRepositoryTests
 //
 //  Created by Hugo Alonso on 17/04/2021.
 //
@@ -50,51 +50,21 @@ class StubCodableInMemoryDataRepositoryImplementation: DataRepository {
     }
 }
 
-
 import XCTest
 @testable import StoreCheckout
 
-class StoreCheckoutTests: XCTestCase {
-
-    func test_products_decodeWhenValid() throws {
-        let json = """
-            {
-            "products": [
-              {
-                "code": "VOUCHER",
-                "name": "Voucher",
-                "price": 5
-              },
-              {
-                "code": "TSHIRT",
-                "name": "T-Shirt",
-                "price": 20
-              },
-              {
-                "code": "MUG",
-                "name": "Coffee Mug",
-                "price": 7.5
-              }
-            ]
-          }
-        """
-
-        let data = json.data(using: .utf8)!
-        let products: Products = try! JSONDecoder()
-            .decode(Products.self, from: data)
-
-        XCTAssertEqual(products.products.count, 3)
-    }
+class DataRepositoryTests: XCTestCase {
 
     func test_repository_decodeWhenValid() throws {
-        let repository: DataRepository = StubCodableInMemoryDataRepositoryImplementation()
+        let repository = createSUT()
 
         XCTAssertEqual(repository.retrieveProducts().count, 3)
     }
 
-//    // MARK: Helpers
-//    func createSUT() -> ([Product]){
-//
-//    }
+    // MARK: Helpers
+
+    func createSUT() -> DataRepository {
+        return StubCodableInMemoryDataRepositoryImplementation()
+    }
 
 }
