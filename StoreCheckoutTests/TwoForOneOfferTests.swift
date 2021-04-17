7c101dc356c5c231a1bf2ce9f0f21c16430395dd
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 15:32:04 +0200

    Two for One returns Zero on different products

--- StoreCheckoutTests/TwoForOneOfferTests.swift
+++ StoreCheckoutTests/TwoForOneOfferTests.swift
@@ -21,4 +21,14 @@ class TwoForOneOfferTests: XCTestCase {
 
         XCTAssertEqual(offer.discount(over: []), 0)
     }
+
+    func test_offer_returnsZeroOnDifferentProducts() throws {
+        let offer = TwoForOneOffer()
+        let products = [
+            StoreProduct(code: "VOUCHER", name: "Voucher", price: 5),
+            StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20),
+            StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
+        ]
+        XCTAssertEqual(offer.discount(over:products), 0)
+    }
 }
