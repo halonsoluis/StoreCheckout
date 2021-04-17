e9248a2271ada680af0810818582fa0e6c683489
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 09:01:57 +0200

    Use a stubbed repository with codable implementation to improve expressivity in pos of real implemementation

--- StoreCheckoutTests/StoreCheckoutTests.swift
+++ StoreCheckoutTests/StoreCheckoutTests.swift
@@ -15,6 +15,42 @@ struct Products: Codable {
     let products: [Product]
 }
 
+protocol DataRepository {
+    func retrieveProducts() -> [Product]
+}
+
+class StubCodableInMemoryDataRepositoryImplementation: DataRepository {
+    let json = """
+        {
+        "products": [
+          {
+            "code": "VOUCHER",
+            "name": "Voucher",
+            "price": 5
+          },
+          {
+            "code": "TSHIRT",
+            "name": "T-Shirt",
+            "price": 20
+          },
+          {
+            "code": "MUG",
+            "name": "Coffee Mug",
+            "price": 7.5
+          }
+        ]
+      }
+    """
+
+    func retrieveProducts() -> [Product] {
+        let data = json.data(using: .utf8)!
+        let products: Products = try! JSONDecoder()
+            .decode(Products.self, from: data)
+        return products.products
+    }
+}
+
+
 import XCTest
 @testable import StoreCheckout
 
@@ -42,10 +78,23 @@ class StoreCheckoutTests: XCTestCase {
             ]
           }
         """
+
         let data = json.data(using: .utf8)!
         let products: Products = try! JSONDecoder()
             .decode(Products.self, from: data)
 
         XCTAssertEqual(products.products.count, 3)
     }
+
+    func test_repository_decodeWhenValid() throws {
+        let repository: DataRepository = StubCodableInMemoryDataRepositoryImplementation()
+
+        XCTAssertEqual(repository.retrieveProducts().count, 3)
+    }
+
+//    // MARK: Helpers
+//    func createSUT() -> ([Product]){
+//
+//    }
+
 }
