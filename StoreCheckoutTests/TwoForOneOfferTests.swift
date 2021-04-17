8e1b1bcf34f7b76163b3dc4922c4226ed350c38c
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 20:15:45 +0200

    Apply discount of offer for 2 for one when there's more than 3 elements of the same type

--- StoreCheckoutTests/TwoForOneOfferTests.swift
+++ StoreCheckoutTests/TwoForOneOfferTests.swift
@@ -9,20 +9,35 @@ import XCTest
 @testable import StoreCheckout
 
 class TwoForOneOffer: Offer {
-    
     func discount(over products: [StoreProduct]) -> Float {
-        let uniqueProducts = Array(Set(products))
-        var repeatedProducts = products
-        uniqueProducts.forEach { product in
-            if let index = repeatedProducts.firstIndex(of: product) {
-                repeatedProducts.remove(at: index)
+
+        let amountOfProducts = amountByProduct(products: products)
+
+        return amountOfProducts.map { (product, count) in
+            let repeatedCount = Int(count / 2)
+            return product.price * Float(repeatedCount)
+        }.reduce(0, +)
+    }
+
+    func amountByProduct(products: [StoreProduct]) -> [StoreProduct: Int] {
+        var amountByProduct: [StoreProduct: Int] = [:]
+        products.forEach { product in
+            if amountByProduct[product] == nil {
+                amountByProduct[product] = 1
+            } else {
+                amountByProduct[product] = amountByProduct[product]?.advanced(by: 1)
             }
         }
-        return repeatedProducts.map(\.price).reduce(0, +)
+        return amountByProduct
     }
 }
 
 class TwoForOneOfferTests: XCTestCase {
+
+    let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
+    let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
+    let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
+    let mug2 = StoreProduct(code: "MUG-2", name: "Coffee Mug", price: 7.5)
 
     func test_offer_returnsNoDiscountOnEmptyBasket() throws {
         let offer = TwoForOneOffer()
@@ -32,23 +47,35 @@ class TwoForOneOfferTests: XCTestCase {
 
     func test_offer_returnsZeroOnDifferentProducts() throws {
         let offer = TwoForOneOffer()
-        let products = [
-            StoreProduct(code: "VOUCHER", name: "Voucher", price: 5),
-            StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20),
-            StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-        ]
+        let products = [voucher, tShirt, mug]
         XCTAssertEqual(offer.discount(over:products), 0)
     }
 
-    func test_offer_returnsFullPriceOfRepeatedProducts() throws {
+    func test_offer_returnsAmountOfRepeatedProducts() throws {
         let offer = TwoForOneOffer()
-        let products = [
-            StoreProduct(code: "VOUCHER", name: "Voucher", price: 5),
-            StoreProduct(code: "VOUCHER", name: "Voucher", price: 5),
-            StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5),
-            StoreProduct(code: "MUG-2", name: "Coffee Mug", price: 7.5),
-            StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-        ]
+
+        let products = [voucher, voucher, mug, mug2, mug]
+
+        XCTAssertEqual(offer.amountByProduct(products: products), [
+            voucher: 2,
+            mug: 2,
+            mug2: 1
+        ])
+    }
+
+    func test_offer_returnsDiscountOfRepeatedProducts() throws {
+        let offer = TwoForOneOffer()
+
+        let products = [voucher, voucher, mug, mug2, mug]
+
+        XCTAssertEqual(offer.discount(over:products), 12.5)
+    }
+
+    func test_offer_returnsDiscountForTuplesOfRepeatedProducts() throws {
+        let offer = TwoForOneOffer()
+
+        let products = [voucher, voucher, voucher, mug, mug2, mug]
+
         XCTAssertEqual(offer.discount(over:products), 12.5)
     }
 }
