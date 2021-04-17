830fa95d118d55fb52875402343a263b0359a417
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-18 00:37:52 +0200

    Move URLSessionHTTPClient into production

--- StoreCheckoutIntegrationTests/StoreCheckoutIntegrationTests.swift
+++ StoreCheckoutIntegrationTests/StoreCheckoutIntegrationTests.swift
@@ -8,28 +8,6 @@
 import XCTest
 import StoreCheckout
 
-public final class URLSessionHTTPClient: HTTPClient {
-    let session: URLSession
-
-    public init(session: URLSession = .shared) {
-        self.session = session
-    }
-
-    private struct UnexpectedValues: Error {}
-
-    public func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValues()))
-            }
-        }.resume()
-    }
-}
-
 class StoreCheckoutIntegrationTests: XCTestCase {
 
     func test_availableProducts_areLoadedFromRemote() {
