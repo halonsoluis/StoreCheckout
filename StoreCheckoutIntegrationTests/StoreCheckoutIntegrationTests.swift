fcceb71366fe8491693391361b4b0e0cff8524fc
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-18 01:03:41 +0200

    Remove duplication in code

--- StoreCheckoutIntegrationTests/StoreCheckoutIntegrationTests.swift
+++ StoreCheckoutIntegrationTests/StoreCheckoutIntegrationTests.swift
@@ -21,50 +21,58 @@ class StoreCheckoutIntegrationTests: XCTestCase {
     }
 
     func test_productCost_appliesDiscountWhithTwoForOneOffer() throws {
-        let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
-        let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
-        let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-
         let offer = TwoForOneOffer(discountedProduct: ["VOUCHER"])
-        let checkout = Checkout(products: [voucher, voucher, tShirt, mug], offers: [offer])
+        let sut = createSUT(
+            offers: [ offer ],
+            products: [
+                Products.voucher,
+                Products.voucher,
+                Products.tShirt,
+                Products.mug
+            ]
+        )
 
-        XCTAssertEqual(checkout.costBeforeReductions, 37.5)
-        XCTAssertEqual(checkout.costAfterReductions, 32.5, accuracy: 0.001)
-        XCTAssertEqual(checkout.applicableOffers, [offer.name])
+        XCTAssertEqual(sut.costBeforeReductions, 37.5)
+        XCTAssertEqual(sut.costAfterReductions, 32.5, accuracy: 0.001)
+        XCTAssertEqual(sut.applicableOffers, [offer.name])
     }
 
     func test_productCost_appliesDiscountWhithBulkVoucherOffer() throws {
-        let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
-        let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
-        let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-        
         let offer = BulkDiscount(
             config: [BulkDiscount.Config(code: "VOUCHER", newPrice: 3, minimumAmount: 2)]
         )
-        let checkout = Checkout(products: [voucher, voucher, tShirt, mug], offers: [offer])
+        let sut = createSUT(
+            offers: [ offer ],
+            products: [
+                Products.voucher,
+                Products.voucher,
+                Products.tShirt,
+                Products.mug
+            ]
+        )
 
-        XCTAssertEqual(checkout.costBeforeReductions, 37.5)
-        XCTAssertEqual(checkout.costAfterReductions, 33.5, accuracy: 0.001)
-        XCTAssertEqual(checkout.applicableOffers, [offer.name])
+        XCTAssertEqual(sut.costBeforeReductions, 37.5)
+        XCTAssertEqual(sut.costAfterReductions, 33.5, accuracy: 0.001)
+        XCTAssertEqual(sut.applicableOffers, [offer.name])
     }
 
     func test_checkout_returnFinalCostWhenApplyingBothOffers() {
-        let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
-        let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
-        let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
-
         let twoForOneOffer = TwoForOneOffer(discountedProduct: ["VOUCHER"])
         let bulkOffer = BulkDiscount(
             config: [BulkDiscount.Config(code: "TSHIRT", newPrice: 19, minimumAmount: 3)]
         )
-        let checkout = Checkout(
-            products: [voucher, tShirt, mug],
-            offers: [twoForOneOffer, bulkOffer]
+        let sut = createSUT(
+            offers: [ twoForOneOffer, bulkOffer ],
+            products: [
+                Products.voucher,
+                Products.tShirt,
+                Products.mug
+            ]
         )
 
         //        Items: VOUCHER, TSHIRT, MUG
         //        Total: 32.50€
-        XCTAssertEqual(checkout.costAfterReductions, 32.5, accuracy: 0.001)
+        XCTAssertEqual(sut.costAfterReductions, 32.5, accuracy: 0.001)
 
 
 //
@@ -81,11 +89,14 @@ class StoreCheckoutIntegrationTests: XCTestCase {
 
     //MARK: Helpers
 
-    private func touchEndpointForRetrievingProducts() {
-        let client = URLSessionHTTPClient()
-        let sut: DataRepository = DataRepositoryImplementation(client: client)
+    struct Products {
+        static let voucher = StoreProduct(code: "VOUCHER", name: "Voucher", price: 5)
+        static let tShirt = StoreProduct(code: "TSHIRT", name: "T-Shirt", price: 20)
+        static let mug = StoreProduct(code: "MUG", name: "Coffee Mug", price: 7.5)
+    }
 
-        let receivedProducts = retrieveProducts(from: sut)
+    private func createSUT(offers: [Offer], products: [StoreProduct]) -> Checkout {
+        Checkout(products: products, offers: offers)
     }
 
     private func retrieveProducts(from repository: DataRepository, timeout: TimeInterval = 5) -> [StoreProduct] {
