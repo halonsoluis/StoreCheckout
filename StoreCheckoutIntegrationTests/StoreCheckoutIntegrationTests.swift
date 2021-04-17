d3192dd6940e3946aa31dc60fa9162d484ea3d6e
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-18 00:23:34 +0200

    Create test target for running integration tests

---
+++ StoreCheckoutIntegrationTests/StoreCheckoutIntegrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoreCheckoutIntegrationTests.swift
+//  StoreCheckoutIntegrationTests
+//
+//  Created by Hugo Alonso on 18/04/2021.
+//
+
+import XCTest
+import StoreCheckout
+
+class StoreCheckoutIntegrationTests: XCTestCase {
+
+    func test_availableProducts_areLoadedFromRemote() {
+        
+    }
+
+}
