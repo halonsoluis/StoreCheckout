70ed8c3ef16167558dae118edd7966776d972e1c
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-17 08:40:42 +0200

    Initial Commit

---
+++ StoreCheckout/StoreCheckout.h
@@ -0,0 +1,18 @@
+//
+//  StoreCheckout.h
+//  StoreCheckout
+//
+//  Created by Hugo Alonso on 17/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoreCheckout.
+FOUNDATION_EXPORT double StoreCheckoutVersionNumber;
+
+//! Project version string for StoreCheckout.
+FOUNDATION_EXPORT const unsigned char StoreCheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreCheckout/PublicHeader.h>
+
+
